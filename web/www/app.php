14c05248cd4d0774e2c7bda60726a0d4203a1989
gwen-bo <gwen.bogaert@hotmail.com>
2020-06-16 15:00:02 +0200

    initial commit

---
+++ web/www/app.php
@@ -0,0 +1,63 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy as RouteCollectorProxy;
+
+require __DIR__ . '/vendor/autoload.php';
+
+try {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
+} catch (Exception $e) {
+}
+
+date_default_timezone_set('UTC');
+
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
+$app = AppFactory::create();
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+$app->addBodyParsingMiddleware();
+
+/**
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+  ob_start();
+  phpinfo();
+  $content = ob_get_clean();
+  $response->getBody()->write($content);
+  return $response
+          ->withStatus(200);
+});
+
+// send all other routes to our react app
+$app->get('[/{params:.*}]', function(Request $request, Response $response) {
+  $file = __DIR__ . '/html/build/index.html';
+  $response->getBody()->write(file_get_contents($file));
+  return $response
+              ->withStatus(200);
+});
+
+// Run app
+$app->run();
